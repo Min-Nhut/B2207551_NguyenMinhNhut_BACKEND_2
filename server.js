ab8734f0acf9dc05aa577edac4283abedfc301e5
Min-Nhut <nguyennhut2101@gmail.com>
2025-11-01 08:11:55 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const app = require("./app");
-const config = require("./app/config/index");
+const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-//start server
-const POST = config.app.port;
-app.listen(POST, () => {
-    console.log(`Server is running on ${POST}.`);
-});
+async function startServer () {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }catch(error){
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+
+startServer();
